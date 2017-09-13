736bdfddfd7ced798b32cac48602cf3f124581ed
sabiha <sabiha@ipvisionsoft.com>
2017-09-13 17:39:00 +0600

    Custom View1

--- app/src/main/java/com/example/sabiha/weather/CustomView.java
+++ app/src/main/java/com/example/sabiha/weather/CustomView.java
@@ -1,6 +1,9 @@
 package com.example.sabiha.weather;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -10,8 +13,19 @@ import android.view.View;
 
 public class CustomView extends View {
 
+    private Color color;
+    private int labelPosition;
+    private Paint circlePaint;
     public CustomView(Context context, AttributeSet attributeSet)
     {
         super(context, attributeSet);
+        circlePaint = new Paint();
+        TypedArray a = context.getTheme().obtainStyledAttributes(attributeSet, R.styleable.CustomView, 0, 0);
+        try {
+            color = a.getColor(R.styleable.CustomView_circleColor, getResources().getColor(R.id.white));
+            labelPosition = a.getInteger(R.styleable.CustomView_labelPosition, 0);
+        } finally {
+            a.recycle();
+        }
     }
 }
