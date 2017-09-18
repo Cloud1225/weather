8892ca62d3c59453cdc53f1b7c04d40721fe3ae7
sabiha <sabiha@ipvisionsoft.com>
2017-09-18 17:39:45 +0600

    addAnim

--- app/src/main/java/com/example/sabiha/weather/CustomView.java
+++ app/src/main/java/com/example/sabiha/weather/CustomView.java
@@ -20,6 +20,8 @@ public class CustomView extends View {
     private int color;
     private int labelPosition;
     private Paint circlePaint;
+
+    private  double xNew, yNew, xOld, yOld;
     public CustomView(Context context, AttributeSet attributeSet)
     {
         super(context, attributeSet);
@@ -45,6 +47,7 @@ public class CustomView extends View {
         Log.e("CustomView", width +"  "+height);
 
         float radius = width > height ? height - 20 - 15 : width - 20 - 15;
+        Log.e("CustomView_Radius: ", String.valueOf(radius));
         circlePaint.setColor(color);
         circlePaint.setAntiAlias(true);
         circlePaint.setStyle(Paint.Style.STROKE);
@@ -86,5 +89,51 @@ public class CustomView extends View {
         labelPosition = lPosition;
         invalidate();
         requestLayout();
+    }
+
+    public void animCalc(double startTime, double endTime, double presentTime, double centreTime, double radius)
+    {
+        double a, b, angle;
+        double totalTime = endTime - startTime;
+        if(presentTime == startTime)
+        {
+            angle = 0;
+            a = 0;
+            b = 0;
+            xNew = xOld;
+            yNew = yOld;
+        }
+        else if(presentTime == endTime)
+        {
+            angle = 180;
+            a = 2*radius;
+            b = 0;
+            xNew = xOld + 2*radius;
+            yNew = yOld;
+        }
+        else if(centreTime > presentTime)
+        {
+            a = ((centreTime - presentTime)*2*radius)/totalTime;
+            b = Math.sqrt(Math.pow(radius, 2) - Math.pow(a, 2));
+            angle = Math.acos(a/radius);
+            xNew = (radius - a) + xOld;
+            yNew = yOld - b;
+        }
+        else if(centreTime == presentTime)
+        {
+            angle = 90;
+            a = radius;
+            b = radius;
+            xNew = xOld + radius;
+            yNew = yOld - radius;
+        }
+        else
+        {
+            a = ((presentTime - centreTime)*2*radius)/totalTime;
+            b = Math.sqrt(Math.pow(radius, 2) - Math.pow(a, 2));
+            angle = Math.acos(a/radius);
+            xNew = radius + a + xOld;
+            yNew = yOld - b;
+        }
     }
 }
